56434012322ad67059304629cb10d0b3747a875d
Dean Marano <dean.marano@gmail.com>
2020-01-18 18:02:28 -0600

    Create puzzles in service.

--- app/services/puzzles.js
+++ app/services/puzzles.js
@@ -1,6 +1,33 @@
 import Service, { inject as service } from '@ember/service';
 import { isEmpty } from '@ember/utils';
 import { all } from 'rsvp';
+import { makepuzzle, solvepuzzle } from "sudoku";
+
+export function puzzleToString(puzzle) {
+  let retval = "";
+  puzzle.forEach((cell, index)=> {
+    if(cell == null) {
+      puzzle[index] = '0';
+    }
+  });
+  for(let i = 0; i < 9; i++) {
+    retval+= puzzle.slice(i * 9, (i * 9) + 9).join('');
+  }
+  return retval;
+}
+
+export function puzzleStringToId(puzzleString) {
+  return puzzleString.
+    replace(/000000/g, 'a').
+    replace(/00000/g, 'b').
+    replace(/0000/g, 'c').
+    replace(/000/g, 'd').
+    replace(/00/g, 'e');
+}
+
+function puzzleToId(puzzle) {
+  return puzzleStringToId(puzzleToString(puzzle));
+}
 
 export default Service.extend({
   store: service(),
@@ -9,6 +36,19 @@ export default Service.extend({
     this.set("loaded", isEmpty(this.store.peekAll("puzzle")));
   },
   loaded: true,
+  create() {
+    if(!this.givens) {
+      let generatedPuzzle = makepuzzle();
+      let solution = solvepuzzle(generatedPuzzle);
+      let puzzle = this.store.createRecord("puzzle", {
+        id: puzzleToId(generatedPuzzle),
+        givens: puzzleToId(generatedPuzzle),
+        solution: puzzleToString(solution)
+      });
+      return puzzle.save();
+    }
+  },
+
   fetch: async function() {
     let response = await fetch("/boards.json");
     let boards = await response.json();
