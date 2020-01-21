0443c2e41be06d0f4eb46e8808e39d41bb66e722
Dean Marano <dean.marano@gmail.com>
2020-01-20 19:15:31 -0600

    A visual upgrade from a D to a C+.

--- app/components/sudoku-cell.js
+++ app/components/sudoku-cell.js
@@ -1,4 +1,22 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { readOnly } from '@ember/object/computed';
+
 
 export default Component.extend({
+  tagName: 'button',
+  classNames: ['sudoku-cell'],
+  classNameBindings: ['error', 'selected', 'given', 'sameValue'],
+  error: readOnly('cell.error'),
+  selected: readOnly('cell.selected'),
+  given: readOnly('cell.given'),
+  sameValue: computed('cell.displayValue', 'currentlySelectedCell', 'currentlySelectedCell.displayValue', function() {
+    return this.cell.displayValue &&
+      this.currentlySelectedCell &&
+      this.cell.id !== this.currentlySelectedCell.id &&
+      (this.cell.displayValue === this.currentlySelectedCell.displayValue);
+  }),
+  click(){
+    this.select(this.cell);
+  }
 });
